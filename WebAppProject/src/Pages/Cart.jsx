099e8d300bac1ae405087d2804ff3538a5272a5f
Maciej Trznadel <maciej.trznadel24@gmail.com>
2025-01-15 01:15:10 +0100

    HomePage

---
+++ WebAppProject/src/Pages/Cart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cart() {
+  return <h1>Tu bedzie koszyk</h1>;
+}
+
+export default Cart;
---
+++ WebAppProject/src/Pages/Categories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Categories() {
+  return <h1>tu bedzie mozna wybrac kategorie</h1>;
+}
+
+export default Categories;
